25e4a42d227fae42df58ac94de0ebcdaad53b6cb
araiva <kizzaraiva@gmail.com>
2023-04-05 01:12:29 +0700

    feat: wordpress

--- src/requirements/wordpress/tools/entry.sh
+++ src/requirements/wordpress/tools/entry.sh
@@ -4,34 +4,36 @@ sed -i 's/MYSQL_PASSWORD/'"$MYSQL_PASSWORD"'/' /etc/my.cnf
 sed -i 's/DB_HOST/'"$DB_HOST"'/' /etc/my.cnf
 
 # First Setup
-# if [ ! -f wp-config.php ]; then
-#   wp config create \
-#     --dbhost="$DB_HOST" \
-#     --dbname="$DB_NAME" \
-#     --dbuser="$MYSQL_USER" \
-#     --dbpass="$MYSQL_PASSWORD" \
-#     --force \
-#     --allow-root
-#   wp db create \
-#     --allow-root
-#   wp core install \
-#     --url="$DOMAIN_NAME" \
-#     --title="$WP_TITLE" \
-#     --admin_name="$WP_ADMIN" \
-#     --admin_password="$WP_ADMIN_PASSWORD" \
-#     --admin_email="$WP_ADMIN_EMAIL" \
-#     --skip-email \
-#     --allow-root
-#   wp user create \
-#     "$WP_USER" \
-#     "$WP_USER_EMAIL" \
-#     --role=author \
-#     --user_pass="$WP_USER_PASSWORD" \
-#     --allow-root \
-#     --quiet
-# fi
+if [ ! -f wp-config.php ]; then
+  wp config create \
+    --dbhost="$DB_HOST" \
+    --dbname="$DB_NAME" \
+    --dbuser="$MYSQL_USER" \
+    --dbpass="$MYSQL_PASSWORD" \
+    --force \
+    --allow-root
+  wp db create \
+    --allow-root
+  wp core install \
+    --url="$DOMAIN_NAME" \
+    --title="$WP_TITLE" \
+    --admin_name="$WP_ADMIN" \
+    --admin_password="$WP_ADMIN_PASSWORD" \
+    --admin_email="$WP_ADMIN_EMAIL" \
+    --skip-email \
+    --allow-root
+  wp user create \
+    "$WP_USER" \
+    "$WP_USER_EMAIL" \
+    --role=author \
+    --user_pass="$WP_USER_PASSWORD" \
+    --allow-root \
+    --quiet
+  wp option update siteurl "$WP_SCHEME://tsomsa.42.fr:$WP_PORT" --allow-root
+  wp option update home "$WP_SCHEME://tsomsa.42.fr:$WP_PORT" --allow-root
+fi
 
-# Start phhp-fpm
+# Start php-fpm
 DIR="/run/php"
 if [ ! -d "$DIR" ]; then
   mkdir -p "$DIR"
