380b3ae72a16a986a11c8e30658caa4a88abb5e5
araiva <kizzaraiva@gmail.com>
2023-04-05 00:39:41 +0700

    php7.3

--- src/requirements/wordpress/tools/entry.sh
+++ src/requirements/wordpress/tools/entry.sh
@@ -1,10 +1,40 @@
 #!bin/bash
+sed -i 's/MYSQL_USER/'"$MYSQL_USER"'/' /etc/my.cnf
+sed -i 's/MYSQL_PASSWORD/'"$MYSQL_PASSWORD"'/' /etc/my.cnf
+sed -i 's/DB_HOST/'"$DB_HOST"'/' /etc/my.cnf
+
+# First Setup
+# if [ ! -f wp-config.php ]; then
+#   wp config create \
+#     --dbhost="$DB_HOST" \
+#     --dbname="$DB_NAME" \
+#     --dbuser="$MYSQL_USER" \
+#     --dbpass="$MYSQL_PASSWORD" \
+#     --force \
+#     --allow-root
+#   wp db create \
+#     --allow-root
+#   wp core install \
+#     --url="$DOMAIN_NAME" \
+#     --title="$WP_TITLE" \
+#     --admin_name="$WP_ADMIN" \
+#     --admin_password="$WP_ADMIN_PASSWORD" \
+#     --admin_email="$WP_ADMIN_EMAIL" \
+#     --skip-email \
+#     --allow-root
+#   wp user create \
+#     "$WP_USER" \
+#     "$WP_USER_EMAIL" \
+#     --role=author \
+#     --user_pass="$WP_USER_PASSWORD" \
+#     --allow-root \
+#     --quiet
+# fi
+
+# Start phhp-fpm
 DIR="/run/php"
-if [ -d "$DIR" ]; then
-  # Take action if $DIR exists. #
-  rm -rf "$DIR"
-else
-  mkdir "$DIR"
+if [ ! -d "$DIR" ]; then
+  mkdir -p "$DIR"
 fi
 
-/usr/sbin/php-fpm7.4 -F -R
+/usr/sbin/php-fpm7.3 -F -R
